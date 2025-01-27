8c06d1b4f028344291bfac2abe3831ebc6b59785
Gujrati318 <kunalnagar318@gmail.com>
2025-01-27 11:19:54 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,7 @@
+elements = [
+    "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", 
+    "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorus", 
+    "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium"]
+elements.reverse()
+print(elements)
+
